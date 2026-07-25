11ffa9d67b6379a176ea1400a50ba45738ecb9eb
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-26 01:38:31 +0530

    chai.py

---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,5 @@
+print("chai aur python")
+def chai (n):
+    print (n)
+
+    print("Lemon chai")
---
+++ 01_basics/string.py
@@ -0,0 +1,16 @@
+# String text ko store karti hai
+name = "Sushant"
+
+print(name)
+print(type(name))
+
+
+# Dono valid hain
+
+name1 = "Sushant"
+name2 = 'Sushant'
+
+print(name1)
+print(name2)
+
+
